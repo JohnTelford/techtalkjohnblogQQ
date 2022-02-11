f8da4ebea6a474e1b4d0af3dd1afb3b1002eea91
John <john@johntelford.com>
2022-02-10 16:27:21 -0800

    Desired look and function
    Signed-off-by: John <john@johntelford.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tech Talk John Blog Site',
+  tagline: 'Tech is Cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -27,7 +27,15 @@ const config = {
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          routeBasePath: '/', // Serve the blog at the site's root
           showReadingTime: true,
+          blogTitle: 'John Telford bog',
+          blogSidebarCount: 'ALL',
+          blogListComponent: '@theme/BlogListPage',
+          blogPostComponent: '@theme/BlogPostPage',
+          blogTagsListComponent: '@theme/BlogTagsListPage',
+          blogTagsPostsComponent: '@theme/BlogTagsPostsPage',
+
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
@@ -43,13 +51,13 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Tech Talk John Blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
         },
         items: [
-          {
+/*           {
             type: 'doc',
             docId: 'intro',
             position: 'left',
@@ -61,6 +69,7 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
+ */          
         ],
       },
       footer: {
